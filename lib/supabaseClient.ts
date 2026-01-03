43dae397fa6ba14d0fc9ffaa2231b33bb2018474
Eager <eager.come@gmail.com>
2026-01-03 22:58:38 +0800

    Refactor: Move Supabase credentials to env variables

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,7 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Credentials for Supabase project
-const supabaseUrl = 'https://mivjlphkhrtlebymnkah.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pdmpscGhraHJ0bGVieW1ua2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MjU0MzMsImV4cCI6MjA4MTEwMTQzM30.ngfc8RA5Krxb2tMhPm7DJw2AtGMayUksoGDuSfbwNuo';
+// Credentials for Supabase project
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
